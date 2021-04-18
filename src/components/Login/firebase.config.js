09d1ec879102b6f4a33d27a3fb1cf298982afb49
Probir Ghosh <probirghosh@outlook.com>
2021-04-18 22:00:33 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCra_qtR6ovudHCfbNDPWsepun6NhB4X-A",
+    authDomain: "login-bf34b.firebaseapp.com",
+    projectId: "login-bf34b",
+    storageBucket: "login-bf34b.appspot.com",
+    messagingSenderId: "341852231386",
+    appId: "1:341852231386:web:7df7be66ec3cd9c1def7e3"
+  };
+  export default firebaseConfig;
---
+++ src/pages/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAS4-CLY8Un87zeyEBipYusKnQ9Qi60CgA",
+  authDomain: "you-and-me-wedding-photography.firebaseapp.com",
+  projectId: "you-and-me-wedding-photography",
+  storageBucket: "you-and-me-wedding-photography.appspot.com",
+  messagingSenderId: "1023255081582",
+  appId: "1:1023255081582:web:9e0f1ce0f57b44b2b82c7f"
+  };
+  export default firebaseConfig;
